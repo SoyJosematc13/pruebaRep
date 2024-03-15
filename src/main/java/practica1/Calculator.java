3431ad7e7e453a9af1cc5903c087053b6e93419f
José Manuel Coleto <jmcoletogalan@uma.es>
2024-03-15 13:22:49 +0100

    Add file src

---
+++ src/main/java/practica1/Calculator.java
@@ -0,0 +1,21 @@
+package practica1;
+
+public class Calculator {
+    public double add(double n1, double n2){
+        return n1+n2;
+    }
+    public double subtract(double n1, double n2){
+        return n1-n2;
+    }
+    public double multiply(double n1, double n2){
+        return n1*n2;
+    }
+    public double divide(double n1, double n2){
+        if (n2==0){
+            throw new IllegalArgumentException("Cannot divide by zero");
+        }
+        return n1/n2;
+    }
+
+}
+
